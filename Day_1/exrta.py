539e3348f44b7cd7216682ed5a72df5fc85875cf
Mehmet Peker <144725712+MehmettPeker@users.noreply.github.com>
2026-03-17 18:11:38 +0300

    1 gün eklemeleri

--- Day_1/exrta.py
+++ Day_1/exrta.py
@@ -4,3 +4,11 @@
 # print("Merhaba)" 
 # Eğer parantezi yanlış yerde kapatırsak yine bize hata mesajı verecektir.
 #Dikkat etmemiz gereken durumlar şunlardır: Önce tırnak açıldıysa tırnak önce kapatılır. parantez önce açıldıysa paranetez önce kapalır.Önce neyi açtıysa onu kapat!
+
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.\n2. Knead the dough for 10 minutes\n3. Add 3g of Salt.\n4. Leave to rise for 2 hours.\n5. Bake at 200 degrees C for 30 minutes.")
+
+
+print("Merhaba " + "Halil")
+print("Merhaba" + " Halil")
+print("Merhaba" + " " + "Halil")
--- Day_1/main.py
+++ Day_1/main.py
@@ -4,3 +4,38 @@ print("Merhaba Dünya!")
 
 print("Hello World!")
 
+#print("Merhaba DÜnya" 
+#SyntaxError: '(' was never closed ==> Parantezi kapatmadın. 
+#SyntaxError Hatası Şu anlama gelmektedir.
+#SyntaxError, kodun yazım kurallarına uymadığı anlamına gelir
+
+
+#print("Merhaba)" 
+#Eğer parantezi yanlış yerde kapatırsak yine bize hata mesajı verecektir.
+#Dikkat etmemiz gereken durumlar şunlardır: Önce tırnak açıldıysa tırnak önce kapatılır. 
+#Parantez önce açıldıysa paranetez önce kapalır.Önce neyi açtıysa onu kapat!
+
+#print("Hello World!")
+#IndentationError: unexpected indent ==> IndentationError: Girinti hatası demektir. 
+#Programlamada gereksiz bir girinti Indentation hatasına neden olur.
+
+
+
+#exam_1.py orneğini göster.
+
+
+#Eğer biz tek print() fonksiyonuyla yapmak isteseydik nasıl yapabilirdik.
+# \n Kullanımı. 
+#extra.py bak. ==> 9 satır.
+
+
+
+#Birleştirme İşlemi (+)
+#Matematiksel olarak toplama işlemi yapmamıza yarar.
+#Metinsel ifadeleride birleştirmemize yarar.
+
+print("Merhaba" + "Halil")
+#Birleşitirme işlemini boşluk bırakmadan yapar.
+#3 Farklı boşluk bırakma yönetmi vardır. extra.py (12-14 satırlar)
+
+
