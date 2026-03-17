e2066407cd030ac5633eaef2f7e6cbe870606c77
Mehmet Peker <144725712+MehmettPeker@users.noreply.github.com>
2026-03-17 17:16:02 +0300

    stringler

---
+++ Day_1/exrta.py
@@ -0,0 +1,6 @@
+# print("Merhaba DÜnya" 
+# #SyntaxError: '(' was never closed Parantezi kapatmadın. 
+
+# print("Merhaba)" 
+# Eğer parantezi yanlış yerde kapatırsak yine bize hata mesajı verecektir.
+#Dikkat etmemiz gereken durumlar şunlardır: Önce tırnak açıldıysa tırnak önce kapatılır. parantez önce açıldıysa paranetez önce kapalır.Önce neyi açtıysa onu kapat!
--- Day_1/notes.md
+++ Day_1/notes.md
@@ -1,3 +1,9 @@
 Bugün Öğrendiklerim:
 
 1) print() fonksiyonu öğrendim.
+
+2) Dikkat etmemiz gereken durumlar şunlardır: a) Önce tırnak açıldıysa tırnak önce kapatılır. 
+b) parantez önce açıldıysa paranetez önce kapalır.
+
+####Yani şu şekilde akılda tutulabilir.####
+Önce neyi açtıysa onu kapat!
