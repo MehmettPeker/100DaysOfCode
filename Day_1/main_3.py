8acf47c2490098cd8756b2228aeb947f32802ee8
Mehmet Peker <144725712+MehmettPeker@users.noreply.github.com>
2026-03-17 19:11:17 +0300

    hashtag kullanımı

--- Day_1/main_3.py
+++ Day_1/main_3.py
@@ -1,17 +1,4 @@
 
-#input() fonksiyonu ==> Kullanıcıdan veri almak için kullanılır.
-#print() fonksiyonunda ekrana yazdırma işlemi yapar.
-#input() fonksiyonunda ise kullanıcının bir veri girmesini isteriz.
-#İsminiz nedir? , Nerelisin?, Kaç yaşındasın, Kullanıcı adınız giriniz?, Şifrenizi giriniz?
-
-print("What is your name?")
-
-input("What is your name?")
-
-#Soru ==> Kullanıcıdan ismini isteyip Merhaba Mehmet! çıktısını veren programı yazınız? (Artılı soru)
-# Çözüm exam_3.py de
-
-
 #YORUM SATIRI EKLEME
 
 #Tekli satırlarda yorum satırı yapmak için kullanılır.
