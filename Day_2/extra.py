4e9d904c8d3a760af920ddd98f6b29c9bcb5a3bf
Mehmet Peker <144725712+MehmettPeker@users.noreply.github.com>
2026-03-18 20:20:08 +0300

    Day2

---
+++ Day_2/extra.py
@@ -0,0 +1,41 @@
+print("Hello"[0]) # ==> Dizinin ilk karekterini buluruz.
+
+#String 
+
+print("123"+"456")
+
+#Integer 
+
+print(123 + 456)
+
+#Ek bilgi 
+#Büyük sayıların daha okunaklı olması için tam sayıların arasına _ kullanılabilir.
+
+print(123_456_789)
+
+
+#Float 
+#Ondalıklı sayıların veri tipine denir.
+print(3.14159)
+
+#Boolean
+
+#Yanlızca iki seçeneği olan True ve False değerini alan veri tipidir.
+
+print(True)
+print(False)
+
+
+#type fonksiyonu
+#Verilen ifaden hangi veri tipine gösterir.
+
+type("123") #str
+type(123)   #int
+type(True)  #bool
+type(3.14)  #float
+
+#Veri Dönüşümü
+int("123") 
+print("123"+"456")
+print(int("123")+int("456"))
+str(123)
