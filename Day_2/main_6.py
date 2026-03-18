730414ca13f2cc12ee972e3a2ec1cfad61da4261
Mehmet Peker <144725712+MehmettPeker@users.noreply.github.com>
2026-03-18 23:55:26 +0300

    Day2 son2

---
+++ Day_2/main_6.py
@@ -0,0 +1,32 @@
+# Arttırma Azaltma Çarpma ve Bölme işlemi.
+
+score = 0
+score = score + 1 
+print(score) #1
+#Bir farklı gösterimi kısa yol
+score += 1
+
+
+
+#Azaltma işlemi
+score1 = 1
+score1 = score1 - 1
+print(score1) #0
+#Kısayolu
+score1 -= 1
+
+
+# Çarpma İşlemi
+score2 = 2
+score2 = score2 * 2
+print(score2) #4
+#Kısayolu
+score *= 2
+
+
+# Bölme İşlemi
+score3 = 2
+score3 = score3 / 2
+print(score3) #1
+#Kısayolu
+score3 /= 2
