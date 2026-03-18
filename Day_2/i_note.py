1f90812357310b19bd301d44a4e6bd3e6d19374e
Mehmet Peker <144725712+MehmettPeker@users.noreply.github.com>
2026-03-18 21:27:21 +0300

    Day2.1

---
+++ Day_2/i_note.py
@@ -0,0 +1,12 @@
+# Araştırma sorusu (Gelecek Haftaya öğrenip gelimeleri için) pythonda en yakına yuvarlamak için hangi fonksiyon kullanılır? 
+print(round(3.66))
+
+#Ek Bilgiler
+import math
+
+sayi = 3.66
+
+print(sayi // 1)        # 3.0
+print(round(sayi))      # 4
+print(math.ceil(sayi))  # 4
+print(math.floor(sayi)) # 3
